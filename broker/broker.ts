8da06b0904e918dc1a395dd7ad1a12f24343396a
KaungHtetLwin <kaung.htet.lwin@team.studioamk.com>
2025-10-28 21:21:27 +0630

    update with upstatsh

--- broker/broker.ts
+++ broker/broker.ts
@@ -4,16 +4,25 @@ let theBroker = new Moleculer.ServiceBroker({
   namespace: "StudentManageMentSystem",
   nodeID: "userService",
   transporter: {
-    type: "Redis", 
+    type: "Redis",
+    // options: {
+    //   host: "redis-17181.c8.us-east-1-3.ec2.redns.redis-cloud.com",
+    //   port: 17181,
+    //   password: "F4H39hOAguWlvxw4MWj6TTBVZlV54wXy",
+    //   db: 0,
+    //   tls: {},
+    // },
+
     options: {
-      host: "redis-17181.c8.us-east-1-3.ec2.redns.redis-cloud.com",
-      port: 17181,
+      host: "selected-longhorn-77963-gcp-usc1-search.upstash.io",
+      port: 6379,
       password: "F4H39hOAguWlvxw4MWj6TTBVZlV54wXy",
       db: 0,
       tls: {},
-    },
+    }
+
   },
-  
+
   // transporter: {
   //   type: "TCP",
   //   options: {
@@ -28,15 +37,15 @@ let theBroker = new Moleculer.ServiceBroker({
   logger: console,
   logLevel: "info",
 
-//   created(broker) {
-//     broker.logger.info("created");
-//   },
-//   started(broker) {
-//     broker.logger.info("started");
-//   },
-//   stopped(broker) {
-//     broker.logger.info("stopped");
-//   },
+  //   created(broker) {
+  //     broker.logger.info("created");
+  //   },
+  //   started(broker) {
+  //     broker.logger.info("started");
+  //   },
+  //   stopped(broker) {
+  //     broker.logger.info("stopped");
+  //   },
 });
 
 module.exports = theBroker;

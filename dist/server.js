411a5558b384b320be64b1e4149d3e80af95c5d4
KaungHtetLwin <kaung.htet.lwin@team.studioamk.com>
2025-10-29 19:14:56 +0630

    update

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const ServiceBroker = require("./broker/broker");
+const express = require("express");
 ServiceBroker.options.started = async function (broker) {
     console.log("callback");
 };
@@ -9,5 +10,15 @@ ServiceBroker.start().then(async () => {
     console.log("Started");
     // const result: any = await ServiceBroker.call("sms.user.list");
     // console.log("Result : ", result)
+}).then(() => {
+    const app = express();
+    // app.use("/", (req: Request, res: Response) => {
+    //     console.log('Hello this is nodejs project')
+    //     res.json("This is testing");
+    // });
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, () => {
+        console.log(`Server is listening on  http://localhost:${PORT}`);
+    });
 });
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 const ServiceBroker = require("./broker/broker");
+const express = require("express");
 
 ServiceBroker.options.started = async function (broker: any) {
   console.log("callback");
@@ -9,6 +10,21 @@ ServiceBroker.loadService(__dirname + "/service/service.js");
 ServiceBroker.start().then(async () => {
   console.log("Started");
 
+  
   // const result: any = await ServiceBroker.call("sms.user.list");
   // console.log("Result : ", result)
+}).then(()=>{
+  const app = express();
+
+
+  // app.use("/", (req: Request, res: Response) => {
+  //     console.log('Hello this is nodejs project')
+  //     res.json("This is testing");
+  // });
+
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server is listening on  http://localhost:${PORT}`);
+  });
+
 });
